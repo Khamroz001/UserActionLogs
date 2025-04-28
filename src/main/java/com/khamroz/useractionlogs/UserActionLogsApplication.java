da432c6602b8f57e2ce9f7ff5d88fd1903cb7106
Khamroz001 <khamroz_001@mail.ru>
2025-04-28 22:34:08 +0500

    First commit

---
+++ src/main/java/com/khamroz/useractionlogs/UserActionLogsApplication.java
@@ -0,0 +1,13 @@
+package com.khamroz.useractionlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActionLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActionLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khamroz/useractionlogs/UserActionLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khamroz.useractionlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActionLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
